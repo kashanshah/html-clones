eeed8189920d0c5de80ded66ddc1a45cb237dc2a
Kashan Shah <kashanshah@hotmail.com>
2024-11-08 15:50:46 -0500

    feat: responsive fixes

--- moonshot/index.php
+++ moonshot/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/main.css?v=6" />
+    <link rel="stylesheet" href="./css/main.css?v=7" />
     <script src="js/main.js"></script>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-touch-icon.png">
